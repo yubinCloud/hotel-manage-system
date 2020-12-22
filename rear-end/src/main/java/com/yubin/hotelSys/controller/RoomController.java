837913150a72151591c5c08414f2b6afb133ebf3
yubinCloud <yubin_SkyWalker@yeah.net>
2020-12-22 17:02:29 +0800

    完成查看某天客房预订信息的接口

--- rear-end/src/main/java/com/yubin/hotelSys/controller/RoomController.java
+++ rear-end/src/main/java/com/yubin/hotelSys/controller/RoomController.java
@@ -5,7 +5,11 @@ import com.yubin.hotelSys.result.ExceptionMsg;
 import com.yubin.hotelSys.result.ResponseData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/room")
@@ -17,5 +21,14 @@ public class RoomController {
     @RequestMapping(value = "/show_all")
     public Object showAllRoom() {
         return new ResponseData(ExceptionMsg.SUCCESS, roomMapper.showAllRoom());
+    }
+
+    @RequestMapping(value = "/day_reserve", method = RequestMethod.GET)
+    public Object orderedRoomsOfDay(@RequestParam("year") int year,
+                                    @RequestParam("month") int month,
+                                    @RequestParam("day") int day) {
+        LocalDateTime startTime = LocalDateTime.of(year, month, day, 12, 0);
+        LocalDateTime endTime = startTime.plusDays(1);
+        return new ResponseData(ExceptionMsg.SUCCESS, roomMapper.orderedRooms(startTime, endTime));
     }
 }
--- rear-end/src/main/java/com/yubin/hotelSys/dao/RoomMapper.java
+++ rear-end/src/main/java/com/yubin/hotelSys/dao/RoomMapper.java
@@ -2,8 +2,10 @@ package com.yubin.hotelSys.dao;
 
 import com.yubin.hotelSys.model.Room;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 
@@ -11,5 +13,10 @@ import java.util.List;
 @Repository
 public interface RoomMapper {
 
+    // 展示所有房间
     public List<Room> showAllRoom();
+
+    // 查看某一段时间的已预订房间
+    public List<String> orderedRooms(@Param("startTime") LocalDateTime startTime,
+                                     @Param("endTime") LocalDateTime endTime);
 }
