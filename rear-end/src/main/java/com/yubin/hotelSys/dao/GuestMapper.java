25883cb364055807dc3d52390b21c30385f48a6e
yubinCloud <yubin_SkyWalker@yeah.net>
2020-12-22 21:25:43 +0800

    完成了根据顾客信息去查找顾客

---
+++ rear-end/src/main/java/com/yubin/hotelSys/dao/GuestMapper.java
@@ -0,0 +1,24 @@
+package com.yubin.hotelSys.dao;
+
+import com.yubin.hotelSys.model.Guest;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface GuestMapper {
+
+    // 身份证号精确查询顾客
+    public Guest queryGuestById(@Param("gid") String gid);
+
+    // 根据姓名粗略查找用户
+    public List<Guest> queryGuestByName(@Param("nameRegexp") String nameRegexp);
+
+    // 根据手机号的最后几位去粗略查找用户
+    public List<Guest> queryGuestByPhone(@Param("phoneRegexp") String phoneRegexp);
+}
