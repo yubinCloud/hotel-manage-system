1775c222791d6b1c8dfa627246a7433f3ffa41b6
yubinCloud <yubin_SkyWalker@yeah.net>
2020-12-30 17:20:07 +0800

    a little change

--- rear-end/src/main/java/com/yubin/hotelSys/controller/AdminController.java
+++ rear-end/src/main/java/com/yubin/hotelSys/controller/AdminController.java
@@ -18,7 +18,6 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@CrossOrigin(origins = "*",maxAge = 3600)
 @RequestMapping("/api/admin")
 public class AdminController {
 
--- rear-end/src/main/java/com/yubin/hotelSys/controller/GuestController.java
+++ rear-end/src/main/java/com/yubin/hotelSys/controller/GuestController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/guest")
+@RequestMapping("/api/guest")
 public class GuestController {
     @Autowired
     private GuestMapper guestMapper;
